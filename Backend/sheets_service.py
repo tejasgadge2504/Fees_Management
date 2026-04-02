71726fba95843c5723105d19801f9b1c6bdd2c91
TEJAS GADGE <2022.tejas.gadge@ves.ac.in>
2026-04-02 11:58:25 +0530

    updated sheet service

--- Backend/sheets_service.py
+++ Backend/sheets_service.py
@@ -1,7 +1,26 @@
+import os
+import json
 import gspread
+from google.oauth2.service_account import Credentials
 
-gc = gspread.service_account(filename="service_account.json")
+from dotenv import load_dotenv
+load_dotenv()
+
+SERVICE_ACCOUNT_INFO = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    SERVICE_ACCOUNT_INFO,
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
 
 def get_sheet(sheet_url, sheet_name):
-    sheet = gc.open_by_url(sheet_url)
+    sheet = client.open_by_url(sheet_url)
     return sheet.worksheet(sheet_name)
